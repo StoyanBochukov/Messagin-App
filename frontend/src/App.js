30f71b4d8f333e126dd692489a0c34b4ca543968
Stoyan Bochukov <73019209+StoyanBochukov@users.noreply.github.com>
2023-01-14 00:03:07 +0000

    build: frontend startup - navbar and background

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Background from "./components/Background";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="main">
+      <Background />
+      <div className="nav-wrapper">
+      <Navbar />
+      </div>
+      <div className="wrapper">
+    
+      </div>
+    </div>
+  );
+}
+
+export default App;
